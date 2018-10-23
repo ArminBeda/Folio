8d64da3fa06f29030258a7abefde3e1784952ac3
Beda <BEDAAR@de.softlab.net>
2018-10-23 14:52:59 +0200

    Armin

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+myMap();
+
 function calculate () {
 
 
@@ -118,4 +120,13 @@ function calculate () {
         line = line + 5;
       }
 
+    }
+
+    function myMap() {
+        var mapOptions = {
+            center: new google.maps.LatLng(51.5, -0.12),
+            zoom: 10,
+            mapTypeId: google.maps.MapTypeId.HYBRID
+        }
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
     }
